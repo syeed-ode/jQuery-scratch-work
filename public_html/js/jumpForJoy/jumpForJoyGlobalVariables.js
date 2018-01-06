096e912bcf1c90ef5aa695db870ee8a1d9461ce8
Syeed Ode <sy_ode@yahoo.com>
2018-01-06 00:38:25 -0500

    Completed hovering logic

---
+++ public_html/js/jumpForJoy/jumpForJoyGlobalVariables.js
@@ -0,0 +1,5 @@
+var GUESS_BOX_CLASS = ".guess_box";
+var GUESS_BOX_CLASS_HOVER_CLASS_NM = "my_hover";
+var OPENING_PARAGRAPH = "<p>";
+var CLOSING_PARAGRAPH = "</p>";
+var DISCOUNT_SPAN_ID = 'has_discount';
